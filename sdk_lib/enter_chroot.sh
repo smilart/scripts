908a314b884691468dcb63de9e02fcbbece4b28b
kolychev.a.a <kolychev.a.a@smilart.com>
2016-07-28 15:00:35 +0000

    Updated script for create user smilart

--- sdk_lib/enter_chroot.sh
+++ sdk_lib/enter_chroot.sh
@@ -192,9 +192,8 @@ generate_locales() {
 }
 
 create_user_smilart() {
-    mkdir -p ${FLAGS_chroot}/tmp
-    touch ${FLAGS_chroot}/tmp/create_smilart_user.sh
-    chmod +x /tmp/create_smilart_user.sh
+    touch ${FLAGS_chroot}/etc/create_smilart_user.sh
+    chmod +x /etc/create_smilart_user.sh
     cat <<EOF > /tmp/smilart_sda9/etc/create_smilart_user.sh
  groupadd sudo
  useradd -G sudo,docker -m smilart
@@ -203,9 +202,10 @@ create_user_smilart() {
  sed -i -E "/smilart/s/:[0-9]+:[0-9]+/:0:0/" /etc/passwd
  ln -s /usr/share/baselayout/hosts /etc/hosts
  ls /lib64 > /tmp/docker.log
+ ls /lib64
 EOF
-    chroot "${FLAGS_chroot}" /tmp/create_smilart_user.sh
-    rm ${FLAGS_chroot}/tmp/create_smilart_user.sh 
+    chroot "${FLAGS_chroot}" /etc/create_smilart_user.sh
+    rm ${FLAGS_chroot}/etc/create_smilart_user.sh 
 }
 
 setup_env() {
